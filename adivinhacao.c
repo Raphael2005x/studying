03bd2c87f203464f5c4baad81e461a687e783f23
Raphael <rapharesko@gmail.com>
2025-03-15 00:36:17 -0300

    Comentando linhas que ainda vao ser adicionadas

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,10 +6,12 @@ int main(){
     printf("******************************************\n");
 
     int numerosecreto = 42;
+    int numProxPositivo = 45;
+    int numProxNegativo = 40;
 
     int chute;
 
-    printf("qual é o seu chute? ");
+    printf("Chute um numero: ");
     scanf("%d", &chute);
     printf("Seu chute é %d\n", chute);
 
@@ -17,15 +19,14 @@ int main(){
         printf("Parabéns! Você Acertou!\n");
         printf("Jogue de novo, você é um bom jogador!");
     }
-    else{
+    // else{
 
-        if(chute > numerosecreto){
-            printf("Seu chute foi maior que o numero secreto\n");
-        } 
-        else if (chute < numerosecreto) {
-            printf("Seu chute foi menor que o numero secreto\n");
-        }
-        
+    //     if(chute > numerosecreto){
+    //         printf("Seu chute foi maior que o numero secreto\n");
+    //     } 
+    //     else if (chute < numerosecreto) {
+    //         printf("Seu chute foi menor que o numero secreto\n");
+    //     }
         printf("Você Errou!\n");
         printf("Mas não desanime, Tente de novo!");
     }
