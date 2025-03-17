971c9f05f488bf505dd4c992044edea0d6e09c0a
Raphael <rapharesko@gmail.com>
2025-03-17 16:11:00 -0300

    Comentando o nescessario

--- adivinhacao.c
+++ adivinhacao.c
@@ -7,20 +7,20 @@ int main(){
     printf("* Bem viando ao nosso jogo de advinhação *\n");
     printf("******************************************\n");
 
-    srand(time(NULL));
+    srand(time(NULL)); //inicia o bglh de numero automatico
 
-    int numero_secreto = rand() % 45;
+    int numero_secreto = rand() % 45; // 0 ao 45 numeros aleatorios
     int chute = 0;
     int tentativa, acertou = 0;
 
-    for (tentativa = 1; tentativa <= 10; tentativa++) {
+    for (tentativa = 1; tentativa <= 10; tentativa++) { // ate 10 tentativas
         printf("Tentativa %d: ", tentativa);
         scanf("%d", &chute);
 
         if (chute == numero_secreto) {
             printf("Parabéns! Você acertou o número secreto %d!\n", numero_secreto);
-            acertou = 1;
-            break;
+            acertou = 1; // o codigo dentro do if (!acertou) não será executado
+            break; // se acertar finaliza o codigo.
         } else if (chute < numero_secreto) {
             printf("O número secreto é maior que %d\n", chute);
         } else {
@@ -28,7 +28,7 @@ int main(){
         }
     }
 
-    if (!acertou) {
+    if (!acertou) { //! negação!
         printf("Que pena! Você não conseguiu adivinhar. O número secreto era %d.\n", numero_secreto);
     }
     return 0;
