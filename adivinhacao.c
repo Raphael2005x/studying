d51f50184785e48b7e2d96e831eab42c3877d19a
Raphael <rapharesko@gmail.com>
2025-03-17 16:01:24 -0300

    Agora encerra se acabar as tentativas ou acertar

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
     printf("******************************************\n");
     printf("* Bem viando ao nosso jogo de advinhação *\n");
     printf("******************************************\n");
 
-    int numerosecreto = 42;
-    int numProxPositivo = 45;
-    int numProxNegativo = 40;
+    srand(time(NULL));
 
-    int chute;
+    int numero_secreto = rand() % 45;
+    int chute = 0;
+    int tentativa, acertou = 0;
 
-    printf("Chute um numero: ");
-    scanf("%d", &chute);
-    printf("Seu chute é %d\n", chute);
+    for (tentativa = 1; tentativa <= 10; tentativa++) {
+        printf("Tentativa %d: ", tentativa);
+        scanf("%d", &chute);
 
-    if(chute == numerosecreto) {
-        printf("Parabéns! Você Acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!");
+        if (chute == numero_secreto) {
+            printf("Parabéns! Você acertou o número secreto %d!\n", numero_secreto);
+            acertou = 1;
+            break;
+        } else if (chute < numero_secreto) {
+            printf("O número secreto é maior que %d\n", chute);
+        } else {
+            printf("O número secreto é menor que %d\n", chute);
+        }
     }
-    // else{
 
-    //     if(chute > numerosecreto){
-    //         printf("Seu chute foi maior que o numero secreto\n");
-    //     } 
-    //     else if (chute < numerosecreto) {
-    //         printf("Seu chute foi menor que o numero secreto\n");
-    //     }
-        printf("Você Errou!\n");
-        printf("Mas não desanime, Tente de novo!");
+    if (!acertou) {
+        printf("Que pena! Você não conseguiu adivinhar. O número secreto era %d.\n", numero_secreto);
     }
+    return 0;
 }
+
